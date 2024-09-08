1effc2bbc33435ebbc63253674e5432d7f907bd0
Robert Collier <robert.collier.120@gmail.com>
2024-09-08 19:26:46 -0400

    syntax error fixed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
-app.use("/", () = > {
+app.use("/", () => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
