47a8baf0e10ec8e6ef79c1b3e1e26451fda226a1
Robert Collier <robert.collier.120@gmail.com>
2024-09-08 19:38:38 -0400

    testing server connection changes

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ if (process.env.JAWSDB_URL) {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(routes);
-
 app.use("/", () => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
